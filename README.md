74aaf61d2c6bc592cd156c7d14b9f0917fdcd4ce
CodeWriter21 <CodeWriter21@gmail.com>
2024-03-27 19:28:21 +0330

    We can merge now!

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# PDF-To-Image
-A simple python script for exporting pdf pages as image files.
+PDF-To-Image
+============
+
+A simple python script that helps with doing simple stuff with PDFs.
+
+Functionalities
+---------------
+
+[x] Merge PDFs
+[ ] Split PDFs
+[ ] Export PDF pages as image files
+[ ] Remove pages from a PDF
+[ ] Encrypt a PDF
+[ ] Decrypt a PDF
+[ ] Add watermark to a PDF
+[ ] Export images from a PDF
+[ ] Export text from a PDF
+[ ] Export links from a PDF
+
+Usage
+-----
+
+### Install requirements
+
+Install the most recent version of python for your operating system. Visit [python.org](https://python.org)
+
+Use pip to install the dependencies:
+```bash
+pip install -r requirements.txt
+```
+
+Run the script:
+```bash
+python3 main.py -h
+```
+
+About
+-----
+
+Author: [CodeWriter21](https://github.com/MPCodeWriter21)
+Github: [MPCodeWriter21/PDF-To-Image](https://github.com/MPCodeWriter21/PDF-To-Image)
+
+License
+-------
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE)
---
+++ main.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+import io
+import sys
+from typing import Sequence
+from pathlib import Path
+
+import log21
+
+try:
+    from PyPDF2 import PdfReader, PdfWriter
+except ImportError:
+    from pypdf import PdfReader, PdfWriter
+
+
+def merge_pdfs(
+    input_files: Sequence[str | Path | io.TextIOWrapper],
+    output_stream: io.BufferedWriter
+):
+    """Merge PDF files.
+
+    :param input_files: List of PDF files to concatenate.
+    :param output_stream: Output stream to write to.
+    """
+    try:
+        writer = PdfWriter()
+        for input_file in input_files:
+            log21.info(f'Adding {input_file}...')
+            reader = PdfReader(input_file)
+            for i, page in enumerate(reader.pages):
+                log21.info(f'Adding page {i + 1}...', end='\r')
+                writer.add_page(page)  # type: ignore
+        writer.write(output_stream)
+    finally:
+        output_stream.close()
+
+
+def merge_pdfs_entry_point(
+    output_path: Path,
+    /,
+    *input_paths: Path,
+    force: bool = False,
+    verbose: bool = False
+):
+    """Merge PDF files.
+
+    :param output_path: Path to write concatenated PDF file to.
+    :param input_paths: List of PDF files to concatenate.
+    :param force: Force overwrite of output file.
+    :param verbose: Print verbose output.
+    """
+    if len(input_paths) < 2:
+        log21.critical('Must provide at least two input files.')
+        sys.exit(1)
+    if output_path.exists() and not force:
+        log21.critical('Output file already exists.')
+        sys.exit(1)
+    if verbose:
+        log21.basic_config(level=log21.INFO)
+
+    for path in input_paths:
+        if not path.exists():
+            log21.critical(f'Input file `{path}` does not exist.')
+            sys.exit(1)
+
+    log21.info(f'Concatenating {len(input_paths)} files to {output_path}')
+    with open(output_path, 'wb') as output_file:
+        merge_pdfs(input_paths, output_file)
+
+
+if __name__ == '__main__':
+    if sys.platform == "win32":
+        import os
+        import msvcrt
+        msvcrt.setmode(sys.stdout.fileno(), os.O_BINARY)
+    log21.basic_config(level=log21.ERROR)
+    log21.argumentify({'merge': merge_pdfs_entry_point})
