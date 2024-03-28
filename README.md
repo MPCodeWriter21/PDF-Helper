c7be5fc35ea34c6bcc838327a726ec41d9878ad1
CodeWriter21 <CodeWriter21@gmail.com>
2024-03-28 20:50:14 +0330

    We can remove-pages from a PDF file now.

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 PDF-To-Image
 ============
 
-A simple python script that helps with doing simple stuff with PDFs.
+A simple python script that helps with doing simple stuff with PDFs. It is going to
+become a simple python package after `main.py` reaches 1000 lines of code.
 
 Functionalities
 ---------------
@@ -9,7 +10,7 @@ Functionalities
 + [x] Merge PDFs
 + [ ] Split PDFs
 + [ ] Export PDF pages as image files
-+ [ ] Remove pages from a PDF
++ [x] Remove pages from a PDF
 + [ ] Encrypt a PDF
 + [ ] Decrypt a PDF
 + [ ] Add watermark to a PDF
@@ -29,18 +30,35 @@ Use pip to install the dependencies:
 pip install -r requirements.txt
 ```
 
-Run the script:
+### Merge PDFs
+
+Merge multiple PDFs into one PDF:
 ```bash
-python3 main.py -h
+python3 main.py merge -i <input_file_1> <input_file_2>... <input_file_n> -o <output_file>
+
+# E.g. Merge PDFs 1, 2 and 3 into a new PDF
+python3 main.py merge -i 1.pdf 2.pdf 3.pdf -o new.pdf
+```
+
+### Remove pages from a PDF
+
+Remove pages from a PDF:
+```bash
+python3 main.py remove-pages -i <input_file> -o <output_file> -p <page_number_1>,<page_number_2>,...,<page_number_n>
+
+# E.g. Remove pages 1, 2, 3 and 6 from a PDF
+python3 main.py remove-pages -i 1.pdf -o new.pdf -p 1-3,6
 ```
 
 About
 -----
 
 Author: [CodeWriter21](https://github.com/MPCodeWriter21)
-Github: [MPCodeWriter21/PDF-To-Image](https://github.com/MPCodeWriter21/PDF-To-Image)
+GitHub: [MPCodeWriter21/PDF-To-Image](https://github.com/MPCodeWriter21/PDF-To-Image)
 
 License
 -------
 
-This project is licensed under the MIT License - see the [LICENSE](LICENSE)
+This project is licensed under the MIT License.
+
+see the [LICENSE](LICENSE)
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import io
 import sys
-from typing import Sequence
+from typing import Sequence, Collection
 from pathlib import Path
 
 import log21
@@ -17,17 +17,14 @@ def merge_pdfs(
     :param input_files: List of PDF files to concatenate.
     :param output_stream: Output stream to write to.
     """
-    try:
-        writer = PdfWriter()
-        for input_file in input_files:
-            log21.info(f'Adding {input_file}...')
-            reader = PdfReader(input_file)
-            for i, page in enumerate(reader.pages):
-                log21.info(f'Adding page {i + 1}...', end='\r')
-                writer.add_page(page)  # type: ignore
-        writer.write(output_stream)
-    finally:
-        output_stream.close()
+    writer = PdfWriter()
+    for input_file in input_files:
+        log21.info(f'Adding {input_file}...')
+        reader = PdfReader(input_file)
+        for i, page in enumerate(reader.pages):
+            log21.info(f'Adding page {i + 1}...', end='\r')
+            writer.add_page(page)  # type: ignore
+    writer.write(output_stream)
 
 
 def merge_pdfs_entry_point(
@@ -59,8 +56,101 @@ def merge_pdfs_entry_point(
             sys.exit(1)
 
     log21.info(f'Concatenating {len(input_paths)} files to {output_path}')
-    with open(output_path, 'wb') as output_file:
-        merge_pdfs(input_paths, output_file)
+    try:
+        with open(output_path, 'wb') as output_file:
+            merge_pdfs(input_paths, output_file)
+    except PermissionError:
+        log21.critical(
+            f'Cannot write to output file `{output_path}`.\n'
+            'Check the file permissions and close any applications that may be using '
+            'the file, then try again.'
+        )
+        sys.exit(1)
+
+
+def remove_pages(
+    input_file: str | Path | io.TextIOWrapper, pages_to_remove: Collection[int],
+    output_stream: io.BufferedWriter
+):
+    """Remove pages from a PDF file.
+
+    :param input_file: PDF file to remove pages from.
+    :param pages_to_remove: List of pages to remove.
+    :param output_stream: Output stream to write to.
+    """
+    writer = PdfWriter()
+    reader = PdfReader(input_file)
+    for i, page in enumerate(reader.pages):
+        if i + 1 not in pages_to_remove:
+            log21.info(f'Adding page {i + 1}...', end='\r')
+            writer.add_page(page)  # type: ignore
+    writer.write(output_stream)
+
+
+def parse_pages(pages: str):
+    """Parses the pages string into a list of pages.
+
+    :param pages: The pages string
+        Example: '1-5,7,9-11'
+    :return: A list of pages
+        Example: [1, 2, 3, 4, 5, 7, 9, 10, 11]
+    """
+    pages_ = pages.replace(' ', '')
+    pages_ = pages_.split(',')
+    pages_ = [x.split('-') for x in pages_]
+    pages_ = [x if len(x) == 1 else range(int(x[0]), int(x[1]) + 1) for x in pages_]
+    pages_ = [int(x) for y in pages_ for x in y]
+    return pages_
+
+
+def remove_pages_entry_point(
+    input_path: Path,
+    output_path: Path,
+    pages_to_remove: str,
+    /,
+    force: bool = False,
+    verbose: bool = False
+):
+    """Remove pages from a PDF file.
+
+    :param input_path: Path to PDF file to remove pages from.
+    :param output_path: Path to write PDF file to.
+    :param pages_to_remove: Comma-separated list of pages to remove. Example:
+        '1-5,7,9-11'
+    :param force: Force overwrite of output file.
+    :param verbose: Print verbose output.
+    """
+    if not input_path.exists():
+        log21.critical(f'Input file `{input_path}` does not exist.')
+        sys.exit(1)
+    if output_path.exists() and not force:
+        log21.critical('Output file already exists.')
+        sys.exit(1)
+    if verbose:
+        log21.basic_config(level=log21.INFO)
+
+    try:
+        pages_to_remove_ = parse_pages(pages_to_remove)
+    except ValueError:
+        log21.critical(f'Invalid pages string: `{pages_to_remove}`')
+        sys.exit(1)
+
+    log21.info(
+        'Removing page' + (
+            's ' + ', '.join(str(page) for page in pages_to_remove_[:-1]) +
+            ' and' if len(pages_to_remove_) > 2 else ''
+        ) + f' {pages_to_remove_[-1]} from `{input_path}`'
+    )
+    try:
+        with open(output_path, 'wb') as output_file:
+            remove_pages(input_path, pages_to_remove_, output_file)
+    except PermissionError:
+        log21.critical(
+            f'Cannot write to output file `{output_path}`.\n'
+            'Check the file permissions and close any applications that may be using '
+            'the file, then try again.'
+        )
+        sys.exit(1)
 
 
 if __name__ == '__main__':
@@ -69,4 +159,9 @@ if __name__ == '__main__':
         import msvcrt
         msvcrt.setmode(sys.stdout.fileno(), os.O_BINARY)
     log21.basic_config(level=log21.ERROR)
-    log21.argumentify({'merge': merge_pdfs_entry_point})
+    log21.argumentify(
+        {
+            'merge': merge_pdfs_entry_point,
+            'remove-pages': remove_pages_entry_point
+        }
+    )
